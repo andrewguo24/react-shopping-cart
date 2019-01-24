d6bc2098a5d5f5da8293be9782ddf3257bb65420
Ge Guo <alex@Ges-MacBook-Pro.local>
2019-01-24 21:19:35 +1100

    added increment and delete

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,24 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
-  };
+// class Counter extends Component {
+//   state = {
+//     count: 0,
+//     tags: ["tag1", "tag2", "tag3"]
+//   };
 
-  render() {
-    return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+//         <button className="btn btn-secondary btn-sm">Increment</button>
+//         <ul>
+//           {this.state.tags.map(tag => (
+//             <li key={tag}>{tag}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
 
 //   getBadgeClasses() {
 //     let classes = "badge m-2 badge-";
@@ -32,5 +31,55 @@ class Counter extends Component {
 //     return count === 0 ? "Zero" : count;
 //   }
 // }
+
+// export default Counter;
+
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({
+      value: this.state.value + 1
+    });
+  };
+
+  render() {
+    console.log("Props", this.props.id);
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
 
 export default Counter;
